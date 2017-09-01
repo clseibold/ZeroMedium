5f0191a785d52a2fcd80bc77d42680c000c18f81
Christian Seibold <krixano@outlook.com>
2017-09-01 14:53:30 -0500

    Add Vue.js

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import { sayHello } from "./greet";
+//import { sayHello } from "./greet";
 import { ZeroFrame, SiteInfo, Message } from "./ZeroFrame";
+import * as Vue from "vue/dist/vue.min.js";
 
 //showHello("greeting", "Typescript!");
+
+let app = new Vue({
+    el: "#app",
+    template: '<p>Hello to {{from}}!</p>',
+    data: {
+        from: "[Loading...]"
+    }
+});
 
 class ZeroApp extends ZeroFrame {
     public site_info: SiteInfo;
@@ -9,8 +18,8 @@ class ZeroApp extends ZeroFrame {
     onOpenWebsocket() {
         this.cmd("siteInfo", {}, (site_info: SiteInfo) => {
             this.site_info = site_info;
-            this.showHello("greeting", site_info.auth_address);
-            console.log(site_info);
+            //this.showHello("greeting", site_info.auth_address);
+            app.from = site_info.auth_address;
         });
         this.cmd("wrapperNotification", ["info", "This is still in development!"]);
     }
@@ -18,14 +27,10 @@ class ZeroApp extends ZeroFrame {
     onRequest(cmd: string, message: Message) {
         if (cmd == "setSiteInfo") {
             this.site_info = message.params;
+            app.from = this.site_info.auth_address;
         }
         console.log(message);
     }
-
-    showHello(divName: string, name: string) {
-        const elt = document.getElementById(divName);
-        elt.innerText = sayHello(name);
-    }
 }
 
-let app = new ZeroApp();
+let page = new ZeroApp();
